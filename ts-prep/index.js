7c5f50aed94cb2bf86306fac7ddf50e98bb4f0b1
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-09-11 11:25:18 +0300

    .

---
+++ ts-prep/index.js
@@ -0,0 +1,4 @@
+// const str: string = 'Hello Typescript'
+// let str: string = 'Hello Typescript'
+var str = 'Hello Typescript';
+str = 5;
---
+++ ts-prep/index.ts
@@ -0,0 +1,4 @@
+// const str: string = 'Hello Typescript'
+// let str: string = 'Hello Typescript'
+let str: any = 'Hello Typescript'
+str = 5
